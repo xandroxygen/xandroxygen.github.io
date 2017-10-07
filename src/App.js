a9c624d13404e1e0ec0d31584aa24f3b5d9e750a
Xander Moffatt <xander.moffatt@gmail.com>
2017-10-07 14:02:08 +0300

    style container
    * switch to ems
    * add some text

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import "./App.css"
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app-container">
         <Header title="Xander" subtitle="Moffatt" />
-        <p className="App-intro">This is a work in progress!</p>
+        <p className="app-intro">
+          This is a work in progress! I am a CS student at Brigham Young
+          University, minoring in Music. I'm currently volunteering abroad with
+          my wife! We are teaching English in Russia for the Fall 2017 semester.
+          More things about me and I never know what to say...
+        </p>
       </div>
     )
   }
