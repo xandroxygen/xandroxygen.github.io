7738c4d490c03425d64d73e499ec45ba3d546087
Xander Moffatt <xander.moffatt@gmail.com>
2017-10-11 22:10:43 +0300

    add about section with toggle

---
+++ main.js
@@ -0,0 +1,16 @@
+const introInput = document.getElementById("intro-toggle")
+const conciseText = document.querySelector(".intro-text ul")
+const interestingText = document.querySelectorAll(".intro-text p")
+const conciseLabel = document.getElementById("concise")
+const interestingLabel = document.getElementById("interesting")
+
+introInput.addEventListener("change", function() {
+  const isChecked = introInput.checked
+  conciseText.style.visibility = isChecked ? "hidden" : "visible"
+  interestingText.forEach(function(p) {
+    p.style.visibility = isChecked ? "visible" : "hidden"
+  })
+
+  conciseLabel.style.color = isChecked ? "#888888" : "#5e7ff8"
+  interestingLabel.style.color = isChecked ? "#5e7ff8" : "#888888"
+})
